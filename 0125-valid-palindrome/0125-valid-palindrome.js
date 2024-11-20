370ec97c8bb8e1b869eab0908990de53eb028313
Shahriaralvee <105269086+Shahriaralvee@users.noreply.github.com>
2024-11-20 10:47:46 +0600

    Time: 3 ms (85.34%), Space: 50.5 MB (98.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,6 +2,33 @@
  * @param {string} s
  * @return {boolean}
  */
+// var isPalindrome = function(s) {
+//         if(s === " "){
+//       return true;
+//     }
+//    let str=s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();  
+//  let a=true;
+//     for(let i = 0; i<str.length/2;i++){
+//         if(str[i] != str[str.length-i-1]){
+//             a=false
+            
+            
+//         }
+//         else{
+//             a=true;
+//         }
+        
+//     }
+
+//      if (a && str.length>=1 ){
+//         return true;
+//     }
+//     else {
+//       return false;
+        
+//     }
+    
+// };
 var isPalindrome = function(s) {
      s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let m= s.length;
