537265c0a5520c3765160b2f87144519a84cebcb
Shahriaralvee <105269086+Shahriaralvee@users.noreply.github.com>
2024-11-21 19:42:53 +0600

    Time: 2 ms (41.33%), Space: 48 MB (99.57%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    i =0
+    j=0
+    while (i<s.length && j < t.length){
+        if(s[i]==t[j]){
+            i+=1
+        }
+        j+=1
+    }
+    if(i==s.length){
+        return true
+    }
+    else{
+        return false
+    }
+    
+};
